e45da98bb6a48778d9d309c930b44f2bde86fb32
BbeiyLily <baltimore.leslie@gmail.com>
2018-09-16 17:13:47 +0200

    method

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,7 +8,7 @@
                         <label for="email">Email</label>
                         <input v-model="email" type="text" name="email" id="email" required autofocus>
                         <label for="mdp">Super secret password</label>
-                        <input type="password" name="mdp" id="mdp" v-model="password" required>
+                        <input v-model="password" type="password" name="mdp" id="mdp"  required>
                         <input type="submit" value="Sign in!">
                     </form>
                     <a href="" class="content-link">Forgot your password ? Don't worry, click here :D</a>
@@ -30,6 +30,66 @@ export default {
     return {
       msg: 'Welcome'
     }
+      methods: {
+
+          login: function login() {
+              const { email, password } = this
+              this.$store.dispatch(AUTH_REQUEST, { email, password }).then(() => {
+                  this.$router.push('/')
+              })
+
+              // state - token filed using local storage token and a status field, representing the status of the API call ( loading, success or error )
+              const state = { token: localStorage.getItem('user-token') }
+
+
+              const getters = {
+                  isAuthenticated: state => !!state.token,
+                  authStatus: state => state.status,
+              }
+
+              const actions = {
+                  [AUTH_REQUEST]: ({commit, dispatch}, user) => {
+                      return new Promise((resolve, reject) => { // The Promise used for router redirect in login
+                          commit(AUTH_REQUEST)
+                          axios({url: 'auth', data: user, method: 'POST' })
+                              .then(resp => {
+                                  const token = resp.data.token
+                                  localStorage.setItem('user-token', token) // store the token in localstorage
+                                  commit(AUTH_SUCCESS, token)
+                                  // you have your token, now log in your user :)
+                                  dispatch(USER_REQUEST)
+                                  resolve(resp)
+                              })
+                              .catch(err => {
+                                  commit(AUTH_ERROR, err)
+                                  localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
+                                  reject(err)
+                              })
+                      })
+                  },
+                  [AUTH_LOGOUT]: ({commit, dispatch}) => {
+                      return new Promise((resolve, reject) => {
+                          commit(AUTH_LOGOUT)
+                          localStorage.removeItem('user-token') // clear your user's token from localstorage
+                          resolve()
+                      })
+                  }
+              }
+
+              const mutations = {
+                  [AUTH_REQUEST]: (state) => {
+                      state.status = 'loading'
+                  },
+                  [AUTH_SUCCESS]: (state, token) => {
+                      state.status = 'success'
+                      state.token = token
+                  },
+                  [AUTH_ERROR]: (state) => {
+                      state.status = 'error'
+                  },
+              }
+          }
+      }
   }
 }
 </script>
